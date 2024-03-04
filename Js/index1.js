c2d5258502b006b8cdc6521953737393f5ad1e41
KongJen <supakorn.12.06.2003@gmail.com>
2024-03-04 18:27:22 +0700

    3/4/2024 Js

--- Js/index1.js
+++ Js/index1.js
@@ -16,12 +16,12 @@
 // console.log('My favorite food: %s', favoriteFood);
 
 let online = true;
-let age = 20;
+let Age = 20;
 let fullName = 'KongJen';
 let student = true;
 
 document.getElementById("variable1").textContent = fullName;
-document.getElementById("variable2").textContent = `I'm ${age} years old`;
+document.getElementById("variable2").textContent = `I'm ${Age} years old`;
 document.getElementById("variable3").textContent = `I'm student: ${student}`;
 
 //----------------------------------------------------------------------------------------------//
@@ -54,3 +54,45 @@ let result = 12 % 5 + 8 / 2;
 
 // console.log(result);
 document.getElementById("operators1").textContent = `12 % 5 + 8 / 2: ${result}`;
+
+//----------------------------------------------------------------------------------------------//
+//--User input--//
+
+// This is alert input //
+// let username = window.prompt("What's your username");
+
+// console.log(username);
+
+let username;
+
+document.getElementById("mySubmit").onclick = function(){
+    username = document.getElementById("myText").value;
+    document.getElementById("myh2").textContent = `Hello ${username}.`;
+}
+
+//----------------------------------------------------------------------------------------------//
+//--Type Conversion--//
+
+// let age = window.prompt("How old are you");
+// age = Number(age);
+// age += 1;
+
+// console.log(age, typeof age);
+
+let x;
+let y;
+let z;
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+document.getElementById("conversion1").textContent = `Value x is ${x} ,Type of x is ${typeof x}.`;
+document.getElementById("conversion2").textContent = `Value y is ${y} ,Type of y is ${typeof y}.`;
+document.getElementById("conversion3").textContent = `Value z is ${z} ,Type of z is ${typeof z}.`;
+
+//----------------------------------------------------------------------------------------------//
+//--Constants--//
