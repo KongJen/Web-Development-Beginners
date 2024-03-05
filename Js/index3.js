7282400938a342d1c1918957e08d048478e41e2e
KongJen <supakorn.12.06.2003@gmail.com>
2024-03-05 19:24:06 +0700

    3/5/2024 Js

---
+++ Js/index3.js
@@ -0,0 +1,13 @@
+// TERNARY OPERATION //
+
+
+// let age = 21;
+// let message = age >= 18 ? "You're an adult" : "You're a minor";
+// console.log(message);
+
+let time = 16;
+let greeting = time < 12 ? "Good morning" : "Good afternoon";
+console.log(greeting);
+
+//----------------------------------------------------------------------------------------------//
+// SWITCHES //
