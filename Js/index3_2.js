9f6b4b0133dde964baba1607692b151fe7fabb35
KongJen <supakorn.12.06.2003@gmail.com>
2024-03-08 01:16:29 +0700

    3/8/2024 Js

--- Js/index3_2.js
+++ Js/index3_2.js
@@ -79,3 +79,34 @@ function convert() {
 
 //----------------------------------------------------------------------------------------------//
 // ARRAY //
+
+let fruits = ["apple", "orange", "banana"];
+
+// fruits.push("coconut");
+// fruits.pop();
+// fruits.unshift("mango");
+//fruits.shift();
+
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+
+let numOfFruits = fruits.length;
+let index = fruits.indexOf("orange")
+
+// console.log(index);
+
+for (let i = 0; i < fruits.length; i++) {
+    // console.log(fruits[i]);
+}
+
+fruits.sort().reverse();
+
+for (let fruit of fruits) {
+    // console.log(fruit);
+    
+}
+
+//----------------------------------------------------------------------------------------------//
+// SPEAD OPERATOR //
